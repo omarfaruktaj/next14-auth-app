9d7c9a6a20a4b8dd8d0e40834287335b543644b4
omarfaruktaj <omarfaruk3393@gmail.com>
2024-02-13 14:09:59 +0600

    add register schema

--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -1,3 +1,12 @@
+import * as z from 'zod'
 
-
-
+export const registerSchema = z.object({
+  name: z.string().min(3, { message: 'Please tell us your name.' }),
+  email: z.string().email({ message: 'Please tell us your email' }),
+  password: z
+    .string()
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/, {
+      message:
+        'Please ensure your password is at least 8 characters long and includes uppercase and lowercase letters, a digit, and a special character (@, $, !, %, *, ?, &)',
+    }),
+})
